943262530d6c88066cb8dc08e9befc1c437fe612
Rishi Bachhuka <rishibachhuka5678@gmail.com>
2023-02-08 22:57:39 +0530

    added the dice animation

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ $(".reset").click(function () {
     $(".b1").css("border", "0px solid #ffe300");
     $("#sdoo").css("border", "4px solid #ffe300");
 
+    $("#ys").html("<p>Seven Down</p>");
     $("#ab").html("<p>" + bal + "</p>");
     $(".wintxt").html("Place Your Bet!!");
 });
@@ -15,7 +16,14 @@ $(".reset").click(function () {
 $(".b1").click(function () {
     var idd = this.id;
     //alert(idd);
-    sel=this.id;
+    sel = this.id;
+    if (sel == "supp")
+        $("#ys").html("<p>Seven Up</p>");
+    if (sel == "sdoo")
+        $("#ys").html("<p>Seven Down</p>");
+    if (sel == "sevv")
+        $("#ys").html("<p>Exact Seven</p>");
+
     $(".b1").css("border", "0px solid #ffe300");
     $("#" + this.id).css("border", "4px solid #ffe300");
 
@@ -24,7 +32,7 @@ $(".b1").click(function () {
 $(".bb").click(function () {
     var x = document.getElementById("bet").value;
     var y = parseInt(x);
-    if(bal==0)
+    if (bal == 0)
         alert("please Reset");
     else if (y > bal) {
         alert("Bet a smaller amount");
@@ -32,32 +40,57 @@ $(".bb").click(function () {
     else {
         var a = Math.floor(Math.random() * 6 + 1);
         var b = Math.floor(Math.random() * 6 + 1);
-        //dice animation
+
+        $("#die1").attr("src", "images/dice" + a + ".jpg");
+        $("#die2").attr("src", "images/dice" + b + ".jpg");
+        var ele1 = document.querySelector("#die1");
+        var ele2 = document.querySelector("#die2");
+       
+        
+
+
+        ele1.classList.add("pressed1");
+        ele2.classList.add("pressed2");
 
         var cc = a + b;
-        // alert(cc);
+        //alert(a+" "+b);
         // alert(sel)
+        var outt = " "
         if (cc == 7 && sel == "sevv") {
             bal += y * 2;
-
-            $(".wintxt").html("Congrats You Won");
+            outt = "Congrats You Won";
         }
         else if (cc < 7 && sel == "sdoo") {
             bal += y;
-
-            $(".wintxt").html("Congrats You Won");
+            outt = "Congrats You Won";
         }
         else if (cc > 7 && sel == "supp") {
             bal += y;
-
-            $(".wintxt").html("Congrats You Won");
+            outt = "Congrats You Won";
         }
         else {
             bal -= y;
-
-            $(".wintxt").html("OOPS!! You Lost");
+            outt = "OOPS!! You Lost";
         }
-        $("#ab").html("<p>" + bal + "</p>");
+        outt=outt+" ("+ cc+")";
+        setTimeout(function () {
+            $(".wintxt").html(outt);
+            $("#ab").html("<p>" + bal + "</p>");
+            
+        }, 1000);
+        setTimeout(function () {
+          
+            ele1.classList.remove("pressed1");
+            ele2.classList.remove("pressed2");
+        }, 2500);
+        setTimeout(function () {
+            $(".wintxt").html("Place Your Next Bet");
+
+        }, 2500);
     }
 });
-//firstly 7down will be selected... first take input of which box has to be selected and then change parameters and display accordingly. and then take input of bet size and then if the user presses the bet button then first check if the user has sufficient balance or not. if not then alert the user to enter less amount. if yes then proceed to a different function. here first u randomize 2 variables and then throw the dixce and then when the dice lands then show the win or loose and then change balance.
+
+function anim1()
+{
+
+}
